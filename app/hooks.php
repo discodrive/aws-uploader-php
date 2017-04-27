4cfd244d370d0593bc5004388e021b2a67672509
Lee <lee.a@substrakt.co.uk>
2017-04-27 16:34:24 +0100

    UPDATE: Remove old insert media function

--- app/hooks.php
+++ app/hooks.php
@@ -3,55 +3,7 @@
 use Aws\Exception\AwsException;
 
 add_action('admin_menu', 'apu_admin_menu');
-// add_action( 'wp_handle_upload_prefilter', 'addToAws');
 add_filter('upload_dir', 'filterUploadDir');
-
-// /**
-//  * Intercept the upload process and upload the file to s3 (used with wp_handle_upload_prefilter)
-//  * @return null
-//  */
-// function addToAws($file) {
-// 	global $s3;
-
-// 	if (isset($file)) {
-
-// 		$name 	  = $file['name'];
-// 		$tmp_name = $file['tmp_name'];
-
-// 		$extension =  explode('.', $name);
-// 		$extension = strtolower(end($extension));
-
-// 		// Set temp details
-// 		$key = md5(uniqid());
-// 		$tmp_file_name = "{key}.{extension}";
-// 		$tmp_file_path = get_temp_dir() . "/{tmp_file_name}";
-
-// 		// Move the file
-// 		move_uploaded_file($tmp_name, $tmp_file_path);
-
-// 		try {
-
-// 			$s3->putObject([
-// 				'Bucket' => APU_BUCKET_NAME,
-// 				'Key'	 => "uploads/{$name}", // Where we store the file on s3
-// 				'Body'	 => fopen($tmp_file_path, 'rb'), // Resource (the actual file) and the access details (read or write)
-// 				'ACL'	 => 'public-read' // Access Control Level
-// 			]);
-
-// 			// Remove the temp file from the server
-// 			unlink($tmp_file_path);
-
-// 		} catch(S3Exception $e) {
-// 			die("There was an error uploading the file");
-// 		}
-
-// 		// Get the file from S3 and insert it into the WordPress media library with a reference
-// 		// Do not upload the image to WordPress
-
-// 		$attachment = awsDirectory() . '/uploads/' . $name;
-// 		insertAttachment($attachment);
-// 	}
-// }
 
 /**
  * Change the upload directory
